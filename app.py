4e44290411b784e4e42f8d76547e3b4e116f8c61
GOUMEIDA AHMED SEYFEDDINE <36012779+SeyfGoumeida@users.noreply.github.com>
2022-05-08 12:29:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -313,7 +313,7 @@ expander2.write(cluster)
 #----------------------------------------------------------------------------------------------------------------------------
 st.header("")
 st.markdown("## 📌 NER Named Entity Recognition : ")
-col1, col2 = st.columns([8,2])
+col3,col1,col4, col2 = st.columns([2,6,2,2])
 expander3 = col1.expander('Show deseases plot :', expanded=True)
 expander3.image(linkNER+str(selectedCluster)+".png",use_column_width ="always")
 expander4 = col2.expander('Show deseases table : ')
